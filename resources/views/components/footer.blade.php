74cc24d40f23baf58842700573053a5fc06436f1
Patrik <kirtap9408@gmail.com>
2026-01-22 15:05:05 +0100

    Swiper & Lightbox

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -26,19 +26,26 @@
             <div class="flex flex-col md:flex-row items-center md:items-start justify-between mb-4">
                 <!-- Logo -->
                 <div class="flex mt-6 md:mt-[0px] items-center space-x-4 mx-auto md:mx-0">
-                    <img src="{{asset('images/footer/IMA_logo_kek.png')}}" alt="Pénzügyi és innovációs szolgáltatások" title="Pénzügyi és innovációs szolgáltatások" class="max-h-32">
+                    <img src="{{asset('images/footer/IMA_logo_kek.png')}}" alt="Pénzügyi és innovációs szolgáltatások"
+                         title="Pénzügyi és innovációs szolgáltatások" class="max-h-32">
                 </div>
 
                 <!-- Social Icons -->
                 <div class="flex space-x-2 mt-4 md:mt-0 mx-auto md:mx-0 md:ml-auto">
-                    <a href="https://www.facebook.com/GloszesTarsa?ref=hl" title="Glósz és Tsa Kft. Facebook oldal" target="_blank">
-                        <img src="{{asset('images/footer/fb-footer.png')}}" title="Glósz és Tsa Kft. Facebook oldal" alt="Glósz és Tsa Kft. Facebook oldal" class="h-6">
+                    <a href="https://www.facebook.com/GloszesTarsa?ref=hl" title="Glósz és Tsa Kft. Facebook oldal"
+                       target="_blank">
+                        <img src="{{asset('images/footer/fb-footer.png')}}" title="Glósz és Tsa Kft. Facebook oldal"
+                             alt="Glósz és Tsa Kft. Facebook oldal" class="h-6">
                     </a>
-                    <a href="https://twitter.com/i/flow/login?redirect_after_login=%2FGlosz_es_Tarsa" title="Glósz és Tsa Kft. X oldal" target="_blank">
-                        <img src="{{asset('images/footer/twitter-lablec.png')}}" title="Glósz és Tsa Kft. X oldal" alt="Glósz és Tsa Kft. X oldal" class="h-6">
+                    <a href="https://twitter.com/i/flow/login?redirect_after_login=%2FGlosz_es_Tarsa"
+                       title="Glósz és Tsa Kft. X oldal" target="_blank">
+                        <img src="{{asset('images/footer/twitter-lablec.png')}}" title="Glósz és Tsa Kft. X oldal"
+                             alt="Glósz és Tsa Kft. X oldal" class="h-6">
                     </a>
-                    <a href="https://hu.linkedin.com/company/gl%C3%B3sz-%C3%A9s-%C3%A1rsa-kft." title="Glósz és Tsa Kft. LinkedIn oldal" target="_blank">
-                        <img src="{{asset('images/footer/in-footer.png')}}" title="Glósz és Tsa Kft. LinkedIn oldal" alt="Glósz és Tsa Kft. LinkedIn oldal" class="h-6">
+                    <a href="https://hu.linkedin.com/company/gl%C3%B3sz-%C3%A9s-%C3%A1rsa-kft."
+                       title="Glósz és Tsa Kft. LinkedIn oldal" target="_blank">
+                        <img src="{{asset('images/footer/in-footer.png')}}" title="Glósz és Tsa Kft. LinkedIn oldal"
+                             alt="Glósz és Tsa Kft. LinkedIn oldal" class="h-6">
                     </a>
                 </div>
             </div>
@@ -50,8 +57,11 @@
                 <div class="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-8">
                     <!-- Szolgáltatásaink -->
                     <div class="flex flex-col">
-                        <h3 class="text-md font-semibold text-[#004070] uppercase mb-2 text-center md:text-left">Innovációmenedzsment folyamata - Szolgáltatásaink</h3>
-                        <div class="flex flex-col md:flex-row items-center md:items-start text-center md:text-left md:space-x-8"> <!-- Igazítás megoldása -->
+                        <h3 class="text-md font-semibold text-[#004070] uppercase mb-2 text-center md:text-left">
+                            Innovációmenedzsment folyamata - Szolgáltatásaink</h3>
+                        <div
+                            class="flex flex-col md:flex-row items-center md:items-start text-center md:text-left md:space-x-8">
+                            <!-- Igazítás megoldása -->
                             <div class="flex flex-col space-y-1">
                                 <div>Stratégia tervezés</div>
                                 <div>Pályázati vállalások biztosítása</div>
@@ -76,14 +86,24 @@
                     <div class="flex flex-col items-center md:items-start text-center md:text-left">
                         <h3 class="text-md font-semibold text-[#004070] uppercase mb-2">Rólunk</h3>
                         <ul class="space-y-1 text-md">
-                            <li><a href="https://innovacio-menedzsment.hu/kapcsolat" target="_blank" class="hover:underline">Rólunk - kapcsolat</a></li>
-                            <li><a href="https://innovacio-menedzsment.hu/innovaciomenedzsment-szolgaltatasok" target="_blank" class="hover:underline">Szolgáltatásaink</a></li>
-                            <li><a href="https://innovacio-menedzsment.hu/innovaciomenedzsment-szolgaltatas-csomagok" target="_blank" class="hover:underline">Szolgáltatás csomagjaink</a></li>
-                            <li><a href="https://innovacio-menedzsment.hu/szellemi-alkotas-szellemi-termek-szellemi-tulajdon" target="_blank" class="hover:underline">Szellemi alkotás, szellemi termék</a></li>
-                            <li><a href="https://innovacio-menedzsment.hu/innovaciomenedzsment" target="_blank" class="hover:underline">Innovációmenedzsment</a></li>
-                            <li><a href="https://imakademia.hu/" target="_blank" class="hover:underline">Innovációmenedzsment oktatás</a></li>
-                            <li><a href="https://innovacio-menedzsment.hu/innovacio-menedzsment-felhasznalasi-feltetelek" target="_blank" class="hover:underline">Felhasználási feltételeink</a></li>
-                            <li><a href="https://innovacio-menedzsment.hu/innovacio-menedzsment-adatvedelem" target="_blank" class="hover:underline">Adatvédelmi tájékoztató</a></li>
+                            <li><a href="https://innovacio-menedzsment.hu/kapcsolat" target="_blank"
+                                   class="hover:underline">Rólunk - kapcsolat</a></li>
+                            <li><a href="https://innovacio-menedzsment.hu/innovaciomenedzsment-szolgaltatasok"
+                                   target="_blank" class="hover:underline">Szolgáltatásaink</a></li>
+                            <li><a href="https://innovacio-menedzsment.hu/innovaciomenedzsment-szolgaltatas-csomagok"
+                                   target="_blank" class="hover:underline">Szolgáltatás csomagjaink</a></li>
+                            <li>
+                                <a href="https://innovacio-menedzsment.hu/szellemi-alkotas-szellemi-termek-szellemi-tulajdon"
+                                   target="_blank" class="hover:underline">Szellemi alkotás, szellemi termék</a></li>
+                            <li><a href="https://innovacio-menedzsment.hu/innovaciomenedzsment" target="_blank"
+                                   class="hover:underline">Innovációmenedzsment</a></li>
+                            <li><a href="https://imakademia.hu/" target="_blank" class="hover:underline">Innovációmenedzsment
+                                    oktatás</a></li>
+                            <li>
+                                <a href="https://innovacio-menedzsment.hu/innovacio-menedzsment-felhasznalasi-feltetelek"
+                                   target="_blank" class="hover:underline">Felhasználási feltételeink</a></li>
+                            <li><a href="https://innovacio-menedzsment.hu/innovacio-menedzsment-adatvedelem"
+                                   target="_blank" class="hover:underline">Adatvédelmi tájékoztató</a></li>
                         </ul>
                     </div>
                 </div>
@@ -100,4 +120,28 @@
                 innovációmenedzsment • Minden jog fenntartva!</p>
         </div>
     @endif
+    <script src="https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js"></script>
+    <script>
+        GLightbox({
+            selector: '.js-lightbox',
+            loop: true,
+            touchNavigation: true,
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script>
+        new Swiper('.courseSwiper', {
+            slidesPerView: 1,
+            spaceBetween: 24,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    </script>
 </footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,8 @@
     <link rel="stylesheet" href="{{asset('simple.css')}}">
     <script src='https://cdn.tailwindcss.com'></script>
     <script defer src='https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js'></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.nocaptcha.sitekey') }}"></script>
     <style>
         .custom-label input:checked + svg {
