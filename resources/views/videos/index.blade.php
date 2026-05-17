c8b1543c9429706c915969ab934a4da5dbc18ac9
Patrik <kirtap9408@gmail.com>
2026-05-17 17:52:30 +0200

    Video overlay change

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -145,7 +145,7 @@
                                         </div>
 
                                         <div class="absolute left-8 top-16 text-white">
-                                            <p class="text-sm leading-relaxed opacity-95 max-w-[85%] xl:max-w-[260px] line-clamp-4">
+                                            <p class="text-sm font-bold leading-relaxed opacity-95 max-w-[85%] xl:max-w-[325px] line-clamp-4">
                                                 {{ $video->description }}
                                             </p>
                                         </div>
